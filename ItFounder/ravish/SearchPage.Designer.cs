e201d07ffd33b835854fef1c94e8f85028a16fda
Ashu Garg <ashu.garg@OPTIMUSDOM.COM>
2018-03-01 12:58:22 +0530

    first week module

---
+++ ItFounder/ravish/SearchPage.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ITFounder
+{
+    partial class SearchPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchQueryBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.exitbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultList = new System.Windows.Forms.ListBox();
+            this.errorLable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SearchQueryBox
+            // 
+            this.SearchQueryBox.Location = new System.Drawing.Point(307, 43);
+            this.SearchQueryBox.Name = "SearchQueryBox";
+            this.SearchQueryBox.Size = new System.Drawing.Size(162, 20);
+            this.SearchQueryBox.TabIndex = 0;
+            this.SearchQueryBox.Text = "sec 62 noida\r\n";
+           
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(321, 122);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButtonClick);
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.Location = new System.Drawing.Point(402, 122);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(75, 23);
+            this.exitbutton.TabIndex = 2;
+            this.exitbutton.Text = "Exit";
+            this.exitbutton.UseVisualStyleBackColor = true;
+            this.exitbutton.Click += new System.EventHandler(this.ExitButtonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Location";
+            // 
+            // ResultList
+            // 
+            this.ResultList.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultList.FormattingEnabled = true;
+            this.ResultList.HorizontalScrollbar = true;
+            this.ResultList.ItemHeight = 20;
+            this.ResultList.Location = new System.Drawing.Point(12, 163);
+            this.ResultList.Name = "ResultList";
+            this.ResultList.Size = new System.Drawing.Size(712, 104);
+            this.ResultList.TabIndex = 4;
+         
+            // 
+            // errorLable
+            // 
+            this.errorLable.AutoSize = true;
+            this.errorLable.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.errorLable.Location = new System.Drawing.Point(318, 83);
+            this.errorLable.Name = "errorLable";
+            this.errorLable.Size = new System.Drawing.Size(35, 13);
+            this.errorLable.TabIndex = 5;
+            this.errorLable.Text = "label2";
+            this.errorLable.Visible = false;
+            // 
+            // SearchPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ravish.Properties.Resources.finalimage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(736, 295);
+            this.Controls.Add(this.errorLable);
+            this.Controls.Add(this.ResultList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitbutton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.SearchQueryBox);
+            this.Name = "SearchPage";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SearchQueryBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button exitbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ResultList;
+        private System.Windows.Forms.Label errorLable;
+    }
+}
+
---
+++ ItFounder/ravish/SearchPage.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Xml;
+
+namespace ITFounder
+{
+    public partial class SearchPage : Form
+    {
+        #region Variables
+
+        IList<string> _mCompanyNames;
+        IList<string> _mCompanyAddress;
+        XmlDocument _mResponseDoc;
+        string _mSearchQuery;
+        int _mSerialNum;
+        string _mApikey;
+        string _mNameAddress;
+        string _mUrlToBePassed;
+        bool _mIsNumeric;
+        string _mLocationSplitter;
+
+        public string MSearchQuery { get => _mSearchQuery; set => _mSearchQuery = value; }
+
+        public delegate void UpdateControlsDelegate(string val);
+
+        #endregion
+
+        #region Constructor
+
+        public SearchPage()
+        {
+            _mCompanyNames = new List<string>();
+            _mCompanyAddress = new List<string>();
+            _mNameAddress = string.Empty;
+            this.BackColor = Color.Green;
+            _mResponseDoc = new XmlDocument();
+            InitializeComponent();
+            _mApikey = "&key=AIzaSyCIFY1-JBm_cNht8Lfuncb4jCeXPXWubnA";
+            _mIsNumeric = true;
+            _mSearchQuery = SearchQueryBox.Text;
+
+        }
+
+        #endregion
+
+        #region SearchButton
+
+        /// <summary>
+        /// This method is called when Search Button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchButtonClick(object sender, EventArgs e)
+        {
+            if (ResultList.Items != null && ResultList.Items.Count > 0)
+            {
+                ResultList.Items.Clear();
+            }
+
+            List<string> lstComp = new List<string>();
+            ValidationOnSearchLocation(_mSearchQuery);
+
+
+            _mUrlToBePassed = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=IT+companies+in+" + SearchQuerySplitter(_mSearchQuery) + _mApikey;
+
+            RequestToApi();
+            Result();
+        }
+
+        #endregion
+
+        #region Validation
+
+        /// <summary>
+        /// This method is for the validation over the location enter by the user
+        /// </summary>
+        /// <param name="searchquery">this is the location entered by user</param>
+        private void ValidationOnSearchLocation(string searchquery)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(searchquery))
+                {
+                    errorLable.Text = " !!! Please enter a location";
+                    errorLable.Visible = true;
+                    SearchQueryBox.BackColor = Color.Red;
+                }
+                else
+                {
+                    foreach (char c in searchquery)
+                    {
+                        if (c < '0' || c > '9')
+                            _mIsNumeric = false;
+                    }
+
+                    if (_mIsNumeric == true)
+                    {
+                        errorLable.Text = " !!! Please enter a valid location";
+                        errorLable.Visible = true;
+                        SearchQueryBox.BackColor = Color.Red;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        #endregion
+
+        #region LocationSplitter
+
+        /// <summary>
+        /// This method is splitting the location enterd by user,
+        /// in the fromat in which it can be passed in api request
+        /// </summary>
+        /// <param name="searchquery">Location enter by user</param>
+        /// <returns>It returns the string in the form "word1 + word2 +..."</returns>
+        private string SearchQuerySplitter(string searchquery)
+        {
+            string[] splittedWord = searchquery.Split(' ');
+            foreach (var word in splittedWord)
+            {
+                _mLocationSplitter = _mLocationSplitter + word + "+";
+            }
+            return _mLocationSplitter;
+        }
+
+        #endregion
+
+        #region RequestToApi
+
+        /// <summary>
+        ///  This method is sending request to google places api and getting response in xml format
+        /// </summary>
+        private void RequestToApi()
+        {
+            HttpClient client = new HttpClient();
+            Task.Run(async () =>
+            {
+                HttpResponseMessage response = await client.GetAsync(_mUrlToBePassed);
+                string responseString = await response.Content.ReadAsStringAsync();
+                XmlParsing(responseString);
+
+            });
+        }
+
+        #endregion
+
+        #region XmlParsing
+
+        /// <summary>
+        /// This method is for the parsing of xml response fetch from api
+        /// </summary>
+        /// <param name="responsestring">string send by api</param>
+        private void XmlParsing(string responsestring)
+        {
+            _mResponseDoc.LoadXml(responsestring);
+            XmlNodeList nameList = _mResponseDoc.GetElementsByTagName("name");
+            XmlNodeList addressList = _mResponseDoc.GetElementsByTagName("formatted_address");
+            foreach (XmlNode n in nameList)
+            {
+                _mCompanyNames.Add(n.InnerText);
+            }
+
+            foreach (XmlNode n in addressList)
+            {
+                _mCompanyAddress.Add(n.InnerText);
+            }
+        }
+
+        #endregion
+
+        #region Result
+
+        /// <summary>
+        /// This method is joining two lists Namelist and Addresslist and showing Result
+        /// </summary>
+        private void Result()
+        {
+            _mSerialNum = 1;
+            foreach (var word in _mCompanyNames.Zip(_mCompanyAddress, (companyNamesObj, companyAddressObj) => new { companyNamesObj, companyAddressObj }))
+            {
+                _mNameAddress = (_mSerialNum + "." + word.companyNamesObj + "\n " + word.companyAddressObj + "\n" + "\n");
+                try
+                {
+                    if (ResultList.InvokeRequired)
+                    {
+                        this.BeginInvoke(new UpdateControlsDelegate(UpdateControls), _mNameAddress);
+                    }
+                    else
+                    {
+                        UpdateControls(_mNameAddress);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+                _mSerialNum++;
+            }
+        }
+
+        private void UpdateControls(string val)
+        {
+            ResultList.Items.Add(val);
+        }
+
+        #endregion
+
+        #region ExitButton
+
+        /// <summary>
+        /// This method is for exit button.when user click it will close the tab
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitButtonClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+    }
+}
